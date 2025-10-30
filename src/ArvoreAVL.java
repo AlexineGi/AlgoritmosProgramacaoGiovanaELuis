1a060d06bee491f4a4a4f7d382def356b8a3bab4
AlexineGi <giovana.alexine@gmail.com>
2025-10-29 22:44:02 -0300

    Commit inicial

---
+++ src/ArvoreAVL.java
@@ -0,0 +1,74 @@
+public class ArvoreAVL {
+
+    private No raiz;
+
+    // públicos (obrigatórios)
+    public void inserir(int v) {
+
+    }
+
+    public void remover(int v) {
+
+    }
+
+    public boolean buscar(int v) {
+        return false;
+    }
+
+    public String percursoPreOrdem() {
+        return "";
+    }
+
+    public String percursoEmOrdem() {
+        return "";
+    }
+
+    public String percursoPosOrdem() {
+        return "";
+    }
+
+    public String imprimirArvore() {
+        return "";
+    }
+
+    public boolean contem(int v) {
+        return false;
+    }
+
+    // privados (balanceamento)
+    private int altura(No n) {
+        return 1;
+    }
+
+    private int fatorBalanceamento(No n) {
+        return 1;
+    }
+
+    private No rotacaoDireita(No y) {
+        return new No(1);
+    }
+
+    private No rotacaoEsquerda(No x) {
+        return new No(2);
+    }
+
+    private No inserirRec(No n, int v) {
+        return new No(1);
+    }
+
+    private No removerRec(No n, int v) {
+        return new No(1);
+    }
+
+    private void pre(No n, StringBuilder sb) {
+
+    }
+
+    private void em(No n, StringBuilder sb) {
+
+    }
+
+    private void pos(No n, StringBuilder sb) {
+
+    }
+}
